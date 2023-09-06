c76d3dd3e444eb8292591eafac6a20742bf15b78
SpinBoxx <quentin.mimault@orange.fr>
2023-09-06 11:15:37 +0200

    [FIX] BUild

--- components/modals/update-modal.tsx
+++ components/modals/update-modal.tsx
@@ -65,7 +65,7 @@ export function UpdateModal() {
         <DialogHeader>
           <DialogTitle>Modifier {data?.label} </DialogTitle>
           <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+            Make changes to your profile here. Click save when you&apos;re done.
           </DialogDescription>
         </DialogHeader>
         <Form {...form}>
